0e59e5e6fceb1be5991dce7fff56e4c7f7bbbba6
zzc0de <6apmuk@gmail.com>
2024-02-14 20:52:41 +0300

    refactor poster

--- client/src/components/poster/PreviewPoster.tsx
+++ client/src/components/poster/PreviewPoster.tsx
@@ -17,74 +17,75 @@ const PreviewPoster: FC<IPreviewPosterProps> = ({
 }) => {
   return (
     <div className={style.wrapper}>
-
       <div className={style.portrait}>
         {prevImg ? <img src={prevImg} alt="" /> : <img src={BlankAvatar} />}
       </div>
 
       <div className={style.info}>
-
         <div className={style.name}>
           <span className={style.ru}>{movie?.title_ru}</span>
           <span className={style.en}>{movie?.title_en}</span>
         </div>
 
+        {movie?.year && 
         <div className={style.block}>
-          <span className={style.prevText}>{contentConst.movieYear}</span>
-          {movie.year 
-          ? <span className={style.result}>{movie.year}</span>
-          : <span className={style.nf}>{contentConst.notFilled}</span>}
+        <span className={style.prevText}>{contentConst.movieYear}</span>
+        <span className={style.result}>{movie.year}</span>
         </div>
+        }
 
+        {movie?.country &&
+         <div className={style.block}>
+         <span className={style.prevText}>{contentConst.movieCountry}</span>
+         <span className={style.result}>{movie.country}</span> 
+       </div>
+       }
+        
+        {movie?.time &&
         <div className={style.block}>
-          <span className={style.prevText}>{contentConst.movieCountry}</span>
-          {movie.country 
-          ? <span className={style.result}>{movie.country}</span> 
-          : <span className={style.nf}>{contentConst.notFilled}</span>}
+        <span className={style.prevText}>{contentConst.movieTime}</span>
+        <span className={style.result}>{movie.time}</span>
         </div>
-
-        <div className={style.block}>
-          <span className={style.prevText}>{contentConst.movieTime}</span>
-          {movie.time 
-          ? <span className={style.result}>{movie.time}</span>
-          : <span className={style.nf}>{contentConst.notFilled}</span>}
-        </div>
-
-        <div className={style.block}>
+        }
+        
+        {movie.director &&
+          <div className={style.block}>
           <span className={style.prevText}>{contentConst.movieDirector}</span>
-          {movie.director 
-          ? <span className={style.result}>{movie.director}</span>
-          : <span className={style.nf}>{contentConst.notFilled}</span>}
+          <span className={style.result}>{movie.director}</span>
         </div>
+        }
 
-        <div className={style['desc-info']}>
-          <span className={style.prevText}>{contentConst.movieDescription}</span>
-          {movie.description 
-          ? <span className={style.description}>{movie.description }</span>
-          : <span className={style.nf}>{contentConst.notFilled}</span>}
-        </div>
+        {movie.description &&
+           <div className={style['desc-info']}>
+           <span className={style.prevText}>{contentConst.movieDescription}</span>
+           <span className={style.description}>{movie.description }</span>
+         </div>
+        }
 
-        <div className={style.block}>
-          <span className={style.prevText}>{contentConst.movieCategory}</span>
-          <div className={style['genre-column']}>
-              {movie.genre.length
-              ? movie.genre.map((item) => 
-              <span className={style['list-item']}>{item}</span>)
-              :<span className={style.nf}>{contentConst.notFilled}</span>
-              }
-        </div>
-        </div>
+        {movie.genre.length ?
+           <div className={style.block}>
+           <span className={style.prevText}>{contentConst.movieCategory}</span>
+           <div className={style['genre-column']}>
+               {movie.genre.map((item) => 
+               <span className={style['list-item']}>{item}</span>)
+               }
+         </div>
+         </div>
+        : ''
+        }
 
-        <div className={style.block}>
+        {movie.actors.length ?
+          <div className={style.block}>
           <span className={style.prevText}>{contentConst.movieCast}</span>
           <div className={style['genre-column']}>
-              {movie.actors.length
-              ? movie.actors.map((item) => 
+              {movie.actors.map((item) => 
               <span className={style['list-item']}>{item}</span>)
-              :<span className={style.nf}>{contentConst.notFilled}</span>
               }
-        </div>
-        </div>
+          </div>
+          </div>
+        :''
+        }
+        
         <button
           className={style["btn-submit"]}
           onClick={() => createMovieHandler()}
