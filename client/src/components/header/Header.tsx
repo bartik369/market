2faa5c8db87298f2c64e00bbbdf41d3cdd652e2e
zzc0de <6apmuk@gmail.com>
2024-02-14 16:45:45 +0300

    govnologo done

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -2,12 +2,16 @@ import React, {FC} from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../navigation/Navbar';
 import style from './Header.module.css'
+import Glasses from '../../assets/pics/cinema.svg'
 
 const Header: FC = () => {
     return (
         <div className={style['nav-area']}>
             <div className={style.logo}>
-                logo
+                <Link to={'/'}>
+                <img src={Glasses} alt="" />
+                <span>kino</span>
+                </Link>
             </div>
         <Navbar />
         </div>
