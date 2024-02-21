e2f3c34e6e1899fe7238209bb87660181b04bc81
zzc0de <6apmuk@gmail.com>
2024-02-22 00:49:03 +0300

    redux display errors inprocess

---
+++ server/routes/userRoutes.ts
@@ -0,0 +1,47 @@
+import express, {Request, Response} from 'express';
+import User from '../models/user/User';
+import Token from '../models/user/Token';
+import Password from '../models/user/Password';
+import bcrypt from 'bcrypt';
+
+const router = express.Router();
+
+router.post('/create-user/', async(req: Request, res:Response) => {
+    try {
+        const {email, password} = req.body;
+        const dublicate = await User.findOne({email: email});
+
+        if (dublicate) {
+            return res.status(400).json({message: 'User already exist'})
+        }
+        const hashPassword = await bcrypt.hash(password, 7); 
+        const user = new User({
+            email: email,
+            roles: ['USER'],
+            member: ['START'],
+        })
+        await user.save()
+
+        if (user) {
+            await Password.create({
+                userId: user._id,
+                password: hashPassword,
+            });
+            return res.status(201).json({message: 'User created'})
+        } else {
+            return res.status(400).json({message: 'Invalid user data received'})
+        }
+    } catch (error) {
+    }
+});
+
+router.post('/user/:id', async(req: Request, res:Response) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+});
+
+
+export default router;
