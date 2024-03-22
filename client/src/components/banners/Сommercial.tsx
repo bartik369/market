f5dec2bafb18076ed93cbcfe3044f9ad01ad67b4
zzc0de <6apmuk@gmail.com>
2024-03-22 23:44:35 +0300

    1

--- client/src/components/banners/Сommercial.tsx
+++ client/src/components/banners/Сommercial.tsx
@@ -1,14 +1,16 @@
 import React, {FC} from 'react';
 import style from './Banners.module.css'
+import bannerLeft from '../../assets/pics/banner-left.png';
+import bannerRight from '../../assets/pics/banner-right.png';
 
 const Commercial:FC = () => {
     return (
         <div className={style.commercial}>
             <div className={style.item}>
-                dsdfdsfds
+                <img src={bannerLeft} alt="" />
             </div>
             <div className={style.item}>
-                dsdfdsfds
+            <img src={bannerRight} alt="" />
             </div>
         </div>
     );
